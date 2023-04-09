71dac5981cdb0a0ddc136bb9dfa5e07457aafb41
timonzet <127628701+timonzet@users.noreply.github.com>
2023-04-09 19:03:18 +0300

    changes plane

--- homework_02/plane.py
+++ homework_02/plane.py
@@ -9,7 +9,8 @@ class Plane(Vehicle):
     cargo = 0
     max_cargo = 0
 
-    def __init__(self, max_cargo):
+    def __init__(self, max_cargo, weight, fuel, fuel_consumption):
+        super().__init__(weight, fuel, fuel_consumption)
         self.max_cargo = max_cargo
 
     def load_cargo(self, num):
