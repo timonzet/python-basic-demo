96f272589d9c2b577eddcf0e03ca4ee4afd26de0
timonzet <127628701+timonzet@users.noreply.github.com>
2023-04-09 18:05:00 +0300

    Init homework 2

---
+++ homework_02/car.py
@@ -0,0 +1,11 @@
+"""
+создайте класс `Car`, наследник `Vehicle`
+"""
+from homework_02.base import Vehicle
+from homework_02.engine import Engine
+
+class Car(Vehicle):
+    engine = 0
+
+    def set_engine(self):
+        Engine
---
+++ homework_02/plane.py
@@ -0,0 +1,21 @@
+"""
+создайте класс `Plane`, наследник `Vehicle`
+"""
+from homework_02.base import Vehicle
+from homework_02.exceptions import LowFuelError
+
+
+class Plane(Vehicle):
+    cargo = 0
+    max_cargo = 0
+
+    def __init__(self, max_cargo):
+        self.max_cargo = max_cargo
+
+    def load_cargo(self):
+        pass
+
+    def remove_all_cargo(self):
+        cargonull = self.cargo
+        self.cargo = 0
+        return cargonull
